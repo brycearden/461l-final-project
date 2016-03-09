209649ac0b327f480dc7132d37abf2593446c2ed
Bryce Arden <arden.bryce@gmail.com>
2016-03-08 20:20:11 -0800

    removed some debug statements

--- backend/src/run.py
+++ backend/src/run.py
@@ -2,6 +2,4 @@ import os, sys
 
 sys.path.insert(1, os.path.join(os.path.abspath('.'), 'lib'))
 sys.path.append("/home/brycearden/lib/google_appengine")
-print "running the application"
 import application
-print "successfully made the import!"
