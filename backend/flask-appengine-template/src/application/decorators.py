29a4b78b01be4be940284511907142d30ae6eacc
Bryce Arden <arden.bryce@gmail.com>
2016-03-09 07:30:25 -0800

    run with dev_appserver.py src

---
+++ backend/flask-appengine-template/src/application/decorators.py
@@ -0,0 +1,46 @@
+"""
+decorators.py
+
+Decorators for URL handlers
+
+"""
+
+from functools import wraps
+from google.appengine.api import users
+from flask import redirect, request, abort
+import time, sys
+
+
+def login_required(func):
+    """Requires standard login credentials"""
+    @wraps(func)
+    def decorated_view(*args, **kwargs):
+        if not users.get_current_user():
+            return redirect(users.create_login_url(request.url))
+        return func(*args, **kwargs)
+    return decorated_view
+
+
+def admin_required(func):
+    """Requires App Engine admin credentials"""
+    @wraps(func)
+    def decorated_view(*args, **kwargs):
+        if users.get_current_user():
+            if not users.is_current_user_admin():
+                abort(401)  # Unauthorized
+            return func(*args, **kwargs)
+        return redirect(users.create_login_url(request.url))
+    return decorated_view
+
+def time_job(stream=sys.stdout):
+    """ Prints out execution time of a function """
+    def actual_time_job(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            start = time.time()
+            ret = func(*args, **kwargs)
+            end = time.time()
+            elapsed = end - start
+            stream.write("{} took {} seconds\n".format(func.__name__, elapsed))
+        return wrapper
+    return actual_time_job
