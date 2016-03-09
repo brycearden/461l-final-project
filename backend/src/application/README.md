d71b07e122ac85ea7afb7d1bb62d0034a902dd9d
Bryce Arden <arden.bryce@gmail.com>
2016-03-09 07:31:02 -0800

    removed old project

--- backend/src/application/README.md
+++
@@ -1,3 +0,0 @@
-# JOURNEY APPLICATION
-
-This is where all of our code for the journey application will go.
--- backend/src/application/controllers/UserController.py
+++
@@ -1,8 +0,0 @@
-"""
-Controller function for the UserModel
-"""
-from flask_restful import reqparse, Resource
-
-class UserSelf(Resource):
-    def get(self):
-        return UserModel.getSelf()
