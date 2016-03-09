f4b97581d18676d2d874d0623d02707263b22e9a
Bryce Arden <arden.bryce@gmail.com>
2016-03-09 07:30:25 -0800

    run with dev_appserver.py src

---
+++ backend/flask-appengine-template/src/application/controllers/AuthController.py
@@ -0,0 +1,28 @@
+from google.appengine.api import users, mail
+from google.appengine.runtime.apiproxy_errors import CapabilityDisabledError
+
+from flask import request, render_template, flash, url_for, redirect, abort
+
+from flask_cache import Cache
+
+from application import app
+from ..decorators import login_required, admin_required
+
+# from ..models.PaletteModel import EntryModel, EmailSubscriptionModel
+import datetime
+
+# Flask-Cache (configured to use App Engine Memcache API)
+cache = Cache(app)
+
+
+def login():
+    if not users.get_current_user():
+        return redirect(users.create_login_url(request.url))
+
+    return redirect("");
+
+def logout():
+    if users.get_current_user():
+        return redirect(users.create_logout_url(request.url))
+    return redirect("");
+
---
+++ backend/flask-appengine-template/src/application/models/UserModel.py
@@ -0,0 +1,21 @@
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+class UserModel(ndb.Model):
+    """User Model"""
+
+    @staticmethod
+    def getSelf():
+        return UserModel.format(users.get_current_user())
+
+    @staticmethod
+    def format(user):
+        if user is None:
+            return {}
+
+        return {
+            'email': user.email(),
+            'username': user.nickname(),
+            'user_id': user.user_id(),
+            'subscribed': False
+        }
