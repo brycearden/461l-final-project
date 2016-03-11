ee31ea6d34616912c246c366fa50bd218296acac
Bryce Arden <arden.bryce@gmail.com>
2016-03-11 11:10:05 -0800

    added a front website page to the site. It looks really good

--- backend/flask/src/application/views/public/public_index.py
+++ backend/flask/src/application/views/public/public_index.py
@@ -3,9 +3,13 @@
 from flask.views import View
 
 from flask import redirect, url_for
+from flask import render_template
 
 
 class PublicIndex(View):
 
     def dispatch_request(self):
-        return redirect(url_for('list_examples'))
+        # recomment this line if you want to be able to create objects from a
+        # UI on the website
+        # return redirect(url_for('list_examples'))
+        return render_template('index.html')
