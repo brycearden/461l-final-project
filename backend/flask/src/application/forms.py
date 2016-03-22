08a5fe9895ce3e6e27605eafeda84d49290f9098
Bryce Arden <arden.bryce@gmail.com>
2016-03-21 21:22:18 -0500

    changed some stuff to make setup easier

--- backend/flask/src/application/forms.py
+++ backend/flask/src/application/forms.py
@@ -8,19 +8,20 @@ See: http://flask.pocoo.org/docs/patterns/wtforms/
 
 """
 
-from flaskext import wtf
-from flaskext.wtf import validators
+from flask.ext.wtf import Form
 from wtforms.ext.appengine.ndb import model_form
+from wtforms.fields import TextField, TextAreaField
+from wtforms import validators
 
 from application.models.ExampleModel import ExampleModel
 
 
-class ClassicExampleForm(wtf.Form):
-    example_name = wtf.TextField('Name', validators=[validators.Required()])
-    example_description = wtf.TextAreaField('Description', validators=[validators.Required()])
+class ClassicExampleForm(Form):
+    example_name = TextField('Name', validators=[validators.Required()])
+    example_description = TextAreaField('Description', validators=[validators.Required()])
 
 # App Engine ndb model form example
-ExampleForm = model_form(ExampleModel, wtf.Form, field_args={
+ExampleForm = model_form(ExampleModel, Form, field_args={
     'example_name': dict(validators=[validators.Required()]),
     'example_description': dict(validators=[validators.Required()]),
 })
