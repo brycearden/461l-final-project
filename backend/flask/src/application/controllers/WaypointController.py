c6e3a763556c74cfd98e20fd47a515acbf4a56da
Bryce Arden <arden.bryce@gmail.com>
2016-03-31 14:48:59 -0500

    added backend controller files with a little bit of template code

---
+++ backend/flask/src/application/controllers/WaypointController.py
@@ -0,0 +1,71 @@
+"""
+WaypointController.py
+
+This file serves as the controller for the RESTful API that we use to satisfy
+HTTP requests for information from our backend. It uses the Flask RESTful
+framework to marshal objects with the correct syntax.
+
+"""
+
+from flask_restful import reqparse, marshal_with, Resource, inputs, fields
+
+waypoint_fields = {
+    'lat': fields.Float,
+    'long': fields.Float,
+    # TODO: figure out how to serialize keys with Flask RESTful I think they
+    # are called NestedLists or something like that
+}
+
+class Waypoint(Resorce):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument(
+            'urlParam', type=str, required=True,
+            help='This is an example param from HTTP',
+            location='json',
+        )
+        super(Waypoint, self).__init__()
+
+    @marshal_with(waypoint_fields)
+    def get(self, id):
+        print "We are in the get function of Waypoint"
+
+    @marshal_with(waypoint_fields)
+    def post(self, id):
+        print "We are in the post function of Waypoint"
+
+    @marshal_with(waypoint_fields)
+    def put(self, id):
+        print "We are in the update function of Waypoint"
+
+    @marshal_with(waypoint_fields)
+    def delete(self, id):
+        print "We are in the delete function of Waypoint"
+
+
+class WaypointList(Resorce):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument(
+            'urlParam', type=str, required=True,
+            help='This is an example param from HTTP',
+            location='json',
+        )
+        super(WaypointList, self).__init__()
+
+    @marshal_with(waypoint_fields)
+    def get(self, id):
+        print "We are in the get function of WaypointList"
+
+    @marshal_with(waypoint_fields)
+    def post(self, id):
+        print "We are in the post function of WaypointList"
+
+    @marshal_with(waypoint_fields)
+    def put(self, id):
+        print "We are in the update function of WaypointList"
+
+    @marshal_with(waypoint_fields)
+    def delete(self, id):
+        print "We are in the delete function of WaypointList"
+
