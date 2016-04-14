70d8c5c143fe2dc1cf37ff1a638ede2ccd38c3f7
Bryce Arden <arden.bryce@gmail.com>
2016-04-14 14:44:14 -0700

    it compiles now

--- backend/flask/src/application/controllers/TripWaypointController.py
+++ backend/flask/src/application/controllers/TripWaypointController.py
@@ -4,7 +4,7 @@ from flask_restful import reqparse, marshal_with, Resource, inputs, fields
 import logging
 import datetime
 from ..models.WaypointModel import *
-from ..models.TrpModel import *
+from ..models.TripModel import *
 from fields import KeyField, waypoint_fields, trip_fields, user_fields
 
 class TripWaypointAPI(Resource):
@@ -66,37 +66,4 @@ class TripWaypointAPI(Resource):
 
         t.waypoints.remove(w.key)
         return t
-
-
-class WaypointListAPI(Resource):
-    def parse_args(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument(
-            'lat',
-            type=float,
-            help='latitute value of the Waypoint',
-            location='json',
-        )
-        parser.add_argument(
-            'lon',
-            type=float,
-            help='longitude value of the Waypoint',
-            location='json'
-        )
-        return parser.parse_args()
-
-    @marshal_with(trip_fields)
-    def post(self):
-        args = self.parse_args()
-        try:
-            w = Waypoint()
-
-            if args['lat'] is not None:
-                w.lat = args['lat']
-            if args['lon'] is not None:
-                w.lon = args['lon']
-            w.put()
-        except BaseException as e:
-            abort(500, Error="Exception- {0}".format(e.message))
-        return w
 
