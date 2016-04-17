5b902f6c76abe6910286fdcc75b5365f5e6a22ce
Bryce Arden <arden.bryce@gmail.com>
2016-04-17 15:41:22 -0700

    added List API's

--- backend/flask/src/application/controllers/UserController.py
+++ backend/flask/src/application/controllers/UserController.py
@@ -4,11 +4,9 @@ from flask_restful import reqparse, marshal_with, Resource, inputs, fields
 import logging
 import datetime
 from ..models.UserModel import *
-from fields import KeyField, user_fields, waypoint_fields, trip_fields
+from fields import KeyField, user_fields, waypoint_fields, trip_fields, user_list_fields
 
 
-# UserSelf
-#post gets information about the current user
 class UserAPI(Resource):
     def parse_args(self):
         parser = reqparse.RequestParser()
@@ -91,51 +89,24 @@ class UserAPI(Resource):
             "time": str(datetime.datetime.now()),
         }
 
-class UserListAPI(Resource):
-    def parse_args(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument(
-            'distance',
-            type=float,
-            help='number of miles off of route user is willing to look',
-            location='json',
-        )
-        parser.add_argument(
-            'isleader',
-            type=bool,
-            help='whether or not User is the leader of a Caravan',
-            location='json',
-        )
-        parser.add_argument(
-            'email',
-            type=str,
-            help='Users email',
-            location='json',
-        )
-        return parser.parse_args()
+class UserTripListAPI(Resource):
+    """Returns the List of Trips associated with a User"""
 
-    @marshal_with(user_fields)
-    def post(self):
-        args = self.parse_args()
-        try:
-            u = User()
-            # apply command args
-            if args['distance'] is not None:
-                u.distance = args['distance']
-            if args['email'] is not None:
-                u.email = args['email']
-            if args['isleader'] is not None:
-                u.isleader = args['isleader']
-            # TODO: if there are doubles due to default initialization then do
-            # we have multiple keys pointing to the same thing?
-            #u.key = ndb.Key(User, u.email)
-            u.put()
-        except BaseException as e:
-            abort(500, Error="Exception- {0}".format(e.message))
-        return u
+    @marshal_with(user_list_fields)
+    def get(self, user_id):
+        data = []
+        u = User.get_by_id(user_id)
 
-    @marshal_with(user_fields)
-    def get(self):
-        # TODO: return a list of all Users
-        pass
+        if u is None:
+            abort(404)
+
+        # get the list of objects associated with the User
+        for key in u.trips:
+            t = Trip.get(key)
+            if t is not None:
+                data.append(t)
+
+        return {
+            'trips': data,
+        }
 
