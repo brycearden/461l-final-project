e48a0301b6d96d15a108d36b6e06f6a7358efa70
Bryce Arden <arden.bryce@gmail.com>
2016-04-20 19:08:45 -0500

    added in a variable argument parser so you can tell the runTests which files to run

--- backend/flask/src/application/tests/runTests.py
+++ backend/flask/src/application/tests/runTests.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-
 import glob
 from subprocess import check_call, CalledProcessError
 import contextlib
+import argparse
 
 # nifty little context manager that lets me ignore all exceptions in one line
 @contextlib.contextmanager
@@ -16,7 +16,23 @@ def ignored(*exceptions):
 # port the app server is deployed on
 DEV_APPSERVER = "http://localhost:8080"
 
-for filename in glob.glob("*.yaml"):
+parser = argparse.ArgumentParser(description='runs tests for all file args')
+parser.add_argument('filenames',
+                    metavar='Filenames',
+                    type=str,
+                    default=None,
+                    nargs='*',
+                    help='Filename(s) to run the REST tests for. If you do not \
+                    pass in any filenames, then you will run all of the .yaml \
+                    files in the current directory',
+                    )
+args = parser.parse_args()
+if not args.filenames:
+    flist = glob.glob("*.yaml")
+else:
+    flist = args.filenames
+
+for filename in flist:
     print "STARTING TESTS FOR {}".format(filename)
     with ignored(CalledProcessError):
         check_call(["pyresttest", DEV_APPSERVER, filename])
